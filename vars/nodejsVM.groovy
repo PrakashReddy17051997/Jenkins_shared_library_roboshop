4a49e718cf2f654a012d5a83ccea10b4dcbb1012
orakanti <orakanti@rptu.de>
2026-03-21 23:24:20 +0100

    parameterised the catalogue with component for generic use for application using nodejs vm

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -7,7 +7,7 @@ def call (Map configMap) {
         }
         environment {
             packageVersion = ''
-            nexusUrl = '172.31.31.230:8081'
+            //nexusUrl = '172.31.31.230:8081'
 
         }
         options {
@@ -49,7 +49,7 @@ def call (Map configMap) {
                     steps{
                         sh """
                         ls -la
-                        zip -q -r catalogue.zip ./* -x ".git" -x ".zip"
+                        zip -q -r ${configMap.component}.zip ./* -x ".git" -x ".zip"
                         ls -ltr
                         """
                     }
@@ -59,15 +59,15 @@ def call (Map configMap) {
                         nexusArtifactUploader(
                             nexusVersion: 'nexus3',
                             protocol: 'http',
-                            nexusUrl: "${nexusUrl}",
+                            nexusUrl: pipelineGlobals.nexusURL(),
                             groupId: 'com.roboshop',
                             version: "${packageVersion}",
-                            repository: 'catalogue',
+                            repository:"${configMap.component}",
                             credentialsId: 'nexus-auth',
                             artifacts: [
-                                [artifactId: 'catalogue',
+                                [artifactId: "${configMap.component}",
                                 classifier: '',
-                                file: 'catalogue.zip',
+                                file: "${configMap.component}.zip",
                                 type: 'zip']
                             ]
                         )
@@ -86,7 +86,7 @@ def call (Map configMap) {
                         string(name: 'environment', value: "dev")
                         ]
                 
-                        build job: "catalogue-deploy", wait: true, parameters: params     
+                        build job: "${configMap.component}-deploy", wait: true, parameters: params     
             }
             }
             
